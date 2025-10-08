a5bbea2c2d022df31417d9a0c4c16de01a694f96
Lenora15 <Novaknight12@outlook.com>
2025-10-07 20:48:24 -0400

    update

---
+++ Assets/_Scripts/SlingShot.cs
@@ -0,0 +1,23 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlingShot : MonoBehaviour
+{
+    void OnMouseEnter()
+    {
+
+    }
+    void OnMouseExit() {
+        print("Slingshot:OnMouseExit()");
+    }
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
